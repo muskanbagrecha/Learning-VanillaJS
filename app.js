280a3591622f37ca47d09ca0fb8a9329d70760ef
Muskan Bagrecha <muskanbagrecha04@gmail.com>
2021-11-26 16:19:53 +0530

    Included gif

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 var btnTranslate = document.querySelector('#btn-translate');
 var textareaInput = document.querySelector("#textarea-input");
 var outputDiv = document.querySelector("#output");
+var url = "https://api.funtranslations.com/translate/minion.json"
 
+function errorHandler(error) {
+    console.log(Error);
+    alert("Server is down!")
+}
 
 function clickEventHandler() {
     var inputvalue = textareaInput.value;
-    outputDiv.innerText = inputvalue;
+    fetch(GETTranslationUrl(inputvalue))
+    .then(response => response.json()
+    .then(json => 
+        outputDiv.innerHTML = json.contents.translated))
+    .catch(errorHandler);
 };
+
+function GETTranslationUrl(text){
+    updatedurl =  url + "?text="+text
+    return updatedurl;
+}
 
 btnTranslate.addEventListener("click", clickEventHandler);
 
